daccdf296e38ae3e64ad4a6732436555738a20eb
Gaspar Morales <gasparmorales1@gmail.com>
2022-10-15 18:19:24 -0300

    errores

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from itertools import count
 from customers import Customers
 from departments import Departments
 from stores import Store
 from enterprise import Enterprise
-
+def hay_miembros_repetidos(enterprise, str_section):
+    lista_secciones = enterprise.departments()
+    for seccion in lista_secciones:
+        if seccion == str_section:
+            lista_miembros = seccion.member()
+            print(lista_miembros)
+            sin_duplicar = [*set(lista_miembros)]
+            print(sin_duplicar)
+            break
+    
 if __name__ == '__main__':
     
     # declaracion de objetos customers
@@ -23,9 +33,9 @@ if __name__ == '__main__':
     d1.roles = ["Gerente", "AC", "AC"]
     
     d2 = Departments()
-    d1.section = "I+D"
-    d1.members = ["Juan", "Jorge", "Pedro"]
-    d1.roles = ["Gerente", "Investigador", "Limpieza"]
+    d2.section = "I+D"
+    d2.members = ["Juan", "Jorge", "Pedro", "juan"]
+    d2.roles = ["Gerente", "Investigador", "Limpieza"]
     
     # declaracion de objetos Store
     s1 = Store()
@@ -41,8 +51,9 @@ if __name__ == '__main__':
     e1.name = "Dovah Systems"
     e1.budget = 20000
     e1.creation = "10/12/2022"
-    e1.departments = [d1, d2]
-    e1.customers = [c1, c2]
-    e1.store = [s1, s2]
+    e1.departments = [str(d1), str(d2)]
+    e1.customers = [str(c1), str(c2)]
+    e1.store = [str(s1), str(s2)]
     
-    print(e1)
+    # print(e1)
+    hay_miembros_repetidos(e1, "I+D")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-from customers import Customers
-from departments import Departments
-from stores import Store
+print(e1)
